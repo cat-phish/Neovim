d1e4934713a23a9620140c414d722f3dbb5cbd2a
cat-phish <134035929+cat-phish@users.noreply.github.com>
2025-01-05 22:05:06 -0800

    changing package name back, didn't work

--- flake.nix
+++ flake.nix
@@ -328,7 +328,7 @@
     packageDefinitions = {
       # These are the names of your packages
       # you can include as many as you wish.
-      nixcats = {pkgs, ...}: {
+      nvim = {pkgs, ...}: {
         # they contain a settings set defined above
         # see :help nixCats.flake.outputs.settings
         settings = {
@@ -374,7 +374,7 @@
     };
     # In this section, the main thing you will need to do is change the default package name
     # to the name of the packageDefinitions entry you wish to use as the default.
-    defaultPackageName = "nixcats";
+    defaultPackageName = "nvim";
   in
     # see :help nixCats.flake.outputs.exports
     forEachSystem
